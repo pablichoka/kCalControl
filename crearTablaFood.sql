74e7f2319f7eab521aac57eeadb479cd772aee83
Pablo Ruiz Abellán <pabloruiz16@hotmail.com>
2023-03-21 17:14:50 +0100

    Added to repository experimental querys to insert breakfast

--- crearTablaFood.sql
+++ crearTablaFood.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE food (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre varchar(20) NOT NULL,
-    Proteinas float NOT NULL,
-    Hidratos float NOT NULL,
-    azucares float NULL,
-    Grasas float NOT NULL,
-    saturadas float NULL,
-    tipo varchar(15) NOT NULL,
+    name varchar(20) NOT NULL,
+    prot float NOT NULL,
+    prot_total AS (prot*(typSize*0.01)) PERSISTED,
+    carbs float NOT NULL,
+    carbs_sugars float NULL,
+    carbs_total AS (carbs*(typSize*0.01)) PERSISTED,
+    fats float NOT NULL,
+    fats_sat float NULL,
+    fats_total AS (fats*(typSize*0.01)) PERSISTED,
+    type varchar(15) NOT NULL,
     KCal100 int NOT NULL,
-    porcionTipica int NOT NULL,
-    kCalTotales AS (KCal100*(porcionTipica*0.01)) PERSISTED
+    typSize int NOT NULL,
+    kCalTotal AS (KCal100*(typSize*0.01)) PERSISTED
 );
 
--- insertIntoFood.sql
+++ insertIntoFood.sql
@@ -1,2 +1,26 @@
-﻿INSERT INTO food ( Nombre, Proteinas, Hidratos, azucares, Grasas, saturadas, tipo, KCal100, porcionTipica)
+﻿INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
 VALUES ('Atun (escabeche)', 23.5, 0, 0, 0.6, 0.23, 'Pescado', 101, 70);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Piadina', 7, 53, 5.8, 8.5, 1.3, 'Pan', 315, 80);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Mozzarella', 22, 1, 0.8, 33, 3.1, 'Lacteo', 281, 30);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Patatinas', 3.5, 55, 0.8, 33, 3.1, 'Snack', 545, 40);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Pechuga (pollo)', 22.2, 0, 0, 6.2, 1.907, 'Carne', 146, 120);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Hamburguesa', 16.2, 8.7, 2.7, 13.3, 5.7, 'Carne', 146, 240);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Mostaza', 7, 3.5, 2, 11, 0.6, 'Salsa', 150, 10);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Mayonesa', 0.8, 2.2, 1.4, 61, 9.7, 'Salsa', 554, 15);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Gouda (queso)', 23.4, 0, 0, 27.9, 19.3, 'Lacteo', 345, 30);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Salchichon (cerdo)', 25.6, 1.8, 1.5, 42.6, 17.4, 'Carne', 492.6, 20);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Yogurt bifidus (cereales)', 4.6, 20.9, 12.8, 3.3, 2.5, 'Lacteo', 134, 170);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Pan blanco (tostada)', 8.3, 50.5, 4.3, 1.6, 0.39, 'Pan', 240, 40);
+INSERT INTO food ( name, prot, carbs, carbs_sugars, fats, fats_sat, type, KCal100, typSize)
+VALUES ('Tomate', 0.9, 4.6, 3.23, 0.1, 0, 'Verdura', 19, 125);
